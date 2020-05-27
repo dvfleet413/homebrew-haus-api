cea9a9a862358073f1e362490a56d083f77f649f
github username <github email address>
2020-05-27 16:06:10 -0400

    working on handling GET requests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,20 +1,15 @@
 class RecipesController < ApplicationController
     def index
+        recipes = Recipe.all
+        render json: recipes
     end
 
     def show
-    end
-
-    def new
+        @recipe = Recipe.find_by(id: params[:id])
+        render json: recipe
     end
 
     def create
-    end
-
-    def edit 
-    end
-
-    def update
     end
 
     def destroy
