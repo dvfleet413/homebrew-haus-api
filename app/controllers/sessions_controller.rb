adcbfbae9c2f71db49fc9f8114f1dbf7d843b8ca
github username <github email address>
2020-05-31 01:33:30 -0400

    working on authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
     def create
+        @user = User.find_by(name: params[:username])
+        if @user && @user.authenticate(params[:password])
+            render json: @user
+        else
+            render json: {error: "Invalid Credentials"}, status: :unauthorized
+        end
     end
 
     def destroy
