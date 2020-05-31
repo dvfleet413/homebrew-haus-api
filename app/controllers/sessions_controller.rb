3d0d60d0047902f8c3056ddd358500d85193dd1e
github username <github email address>
2020-05-31 14:27:02 -0400

    working on using JWT for authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,12 +2,21 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(name: params[:username])
         if @user && @user.authenticate(params[:password])
-            render json: @user
+            token = generate_token({id: @user.id})
+            resp = {
+                user: UserSerializer.new(@user).to_serialized_json,
+                jwt: token
+            }
+            render json: resp
         else
             render json: {error: "Invalid Credentials"}, status: :unauthorized
         end
     end
 
     def destroy
+    end
+
+    def get_current_user
+        render json: { user: UserSerializer.new(current_user).to_serialized_json }, status: :ok
     end
 end
