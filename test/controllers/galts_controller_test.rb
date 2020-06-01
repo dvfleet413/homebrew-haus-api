81bdcf3106e99c65e16567d9107398666563817a
github username <github email address>
2020-06-01 00:16:05 -0400

    switched to rspec framework

--- test/controllers/galts_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GaltsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/galt_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GaltTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
