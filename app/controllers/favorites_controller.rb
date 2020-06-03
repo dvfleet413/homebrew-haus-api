3adfb5fa3711d4a49febc2672e57da96fdb03cb0
github username <github email address>
2020-06-02 20:45:47 -0400

    getting ready to build functionality for favoriting recipes

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+    before_action :set_user
+
+    def create
+        # Something like @user.recipes << Recipe.find(params[:id])
+    end
+
+    def destroy
+        # Something like @user.recipes.delete(Recipe.find(params[:id]))
+    end
+
+
+    private
+        def set_user
+            # something like @user = User.find(params[:userId])
+        end
 end
